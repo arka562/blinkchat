05a585c8f43a9b405df84362d5e6efab9d26dcce
Arkaprava <arka792002@gmail.com>
2025-09-13 03:28:45 +0530

    saved from sql injection

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,18 +1,22 @@
 import express from "express";
-import { signup,login,logout } from "../controller/auth.controller.js";
-// import { protectRoute } from "../middleware/auth.middleware.js";
-// import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { signup,login,logout,updateProfile } from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-// router.use(arcjetProtection);
+router.use(arcjetProtection);
+
+router.get("/test",arcjetProtection,(req,res)=>{
+  res.status(200).jsom({message:"bot detected"})
+})
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
-// router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
