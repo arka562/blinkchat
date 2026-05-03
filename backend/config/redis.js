13fa155bd728d4777d55fceaf1619f7dfda668ec
Arkaprava Ghosh <arka792002@gmail.com>
2026-05-03 13:44:02 +0530

    added redis public url from local url

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: "redis://127.0.0.1:6379",
+  url: "redis-cli --tls -u redis://default:gQAAAAAAAb1FAAIgcDE3YThjYjljMWYwMmU0NzBjOThiMTk2MWJkMDVmMTRjYg@tidy-lioness-113989.upstash.io:6379",
 });
 
 redisClient.on("error", (err) =>
